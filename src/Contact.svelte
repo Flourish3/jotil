cdbafed4a561b48b1b9448408ab7d9f4c01911e4
Johan Tillmar <johan.tillmar@gmail.com>
2021-09-10 20:27:40 +0200

    Further work

---
+++ src/Contact.svelte
@@ -0,0 +1,62 @@
+<script context="module" lang="ts">
+    export type ContactInfo = {
+        link: string;
+        name: string;
+        iconLink: string;
+    };
+</script>
+
+<script lang="ts">
+    export let contactInfo: ContactInfo[];
+</script>
+
+<div id="contact-container">
+    <h2 class="grid-header">Kontakt</h2>
+
+    {#each contactInfo as c}
+        <div class="img-cont">
+            <img class="logo" src={c.iconLink} alt={c.name} style="vertical-align:middle" />
+        </div>
+        <div class="link-cont">
+            <a href={c.link}>{c.name}</a>
+        </div>
+    {/each}
+</div>
+
+<style>
+    #contact-container {
+        width: 65%;
+        margin: 0 auto;
+        margin-bottom: 30px;
+        
+        border-style: solid;
+        border-width: 0.13em;
+        border-radius: 15px;
+        
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 35px 35px 35px;
+
+        grid-column-gap: 10px;
+        grid-row-gap: 5px;
+        align-items: center;
+    }
+
+    .grid-header{
+        margin-top: 0;
+        grid-column-start: 1;
+        grid-column-end: 4;
+    }
+
+    .img-cont {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        justify-self: end;
+    }
+
+    .link-cont {
+        grid-column-start: 2;
+        grid-column-end: 4;
+        justify-self: start;
+    }
+</style>
---
+++ src/Main.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import Contact, { ContactInfo } from "./Contact.svelte";
+
+    let contactList: ContactInfo[] = [
+        {
+            link: "mailto:johan.tillmar@protonmail.com",
+            name: "johan.tillmar@protonmail.com",
+            iconLink: "email.svg"
+        },
+        {
+            link: "https://se.linkedin.com/in/johan-tillmar-b7122799",
+            name: "Johan Tillmar",
+            iconLink: "linkedin.svg"
+        }
+    ];
+</script>
+
+<main>
+    <h1>Jotil Konsult</h1>
+    <p>Bedriver konsultverksamhet inom IT</p>
+    <Contact  contactInfo={contactList}/>
+</main>
+
+<style>
+	main {
+        display: grid;
+        grid-template-rows: 1fr 1fr auto;
+		text-align: center;
+		padding: 0.5em;
+		max-width: 240px;
+		margin: 0 auto;
+        width: 70%;
+        background-color: rgb(205, 225, 226);
+        height: 96%;
+        border-radius: 15px;
+	}
+
+	h1 {
+		color: rgb(36, 37, 49);
+		font-size: 4.5em;
+		font-weight: 1;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
